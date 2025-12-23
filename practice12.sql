9b6b81a0925808cf7da0a455d0fb3a890b6e68d9
romancarcan325-pixel <romancarcan325@gmail.com>
2025-12-23 15:17:46 +0500

    Add files via upload

---
+++ practice12.sql
@@ -0,0 +1,58 @@
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL CHECK (quantity >= 0) -- Количество на складе
+);
+
+CREATE TABLE order_history (
+    log_id SERIAL PRIMARY KEY,
+    product_id INT,
+    quantity_changed INT,
+    notes VARCHAR(255),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO products (product_id, name, quantity) VALUES
+(1, 'Ноутбук', 20),
+(2, 'Смартфон', 50);
+
+BEGIN; -- Задание 1
+
+UPDATE products
+SET quantity = quantity -5
+WHERE product_id = 1;
+
+INSERT INTO order_history (product_id, quantity_changed, notes)
+VALUES (1, -5, 'Продажа 5 ноутбуков');
+
+COMMIT;
+
+SELECT * FROM products
+WHERE product_id = 1;
+SELECT * FROM order_history
+WHERE product_id =1;
+
+BEGIN; -- Задание 2
+
+UPDATE products
+SET quantity = quantity - 100
+WHERE product_id = 2;
+
+INSERT INTO order_history (product_id, quantity_changed, notes)
+VALUES (2, -100, 'Неудалось продать');
+COMMIT;
+SELECT * FROM order_history
+WHERE product_id =2;
+SELECT * FROM products
+WHERE product_id =2;
+
+BEGIN; -- Задание 3
+UPDATE products
+SET quantity = quantity -2
+WHERE product_id = 1;
+
+ROLLBACK;
+
+
+SELECT * FROM products
+WHERE product_id = 1;
